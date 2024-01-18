037fa1d6d47baa12ab090ffd6efb8bad9902c00b
Brian Stevenson <81999388+BStevens1987@users.noreply.github.com>
2024-01-17 20:07:22 -0800

    Change format of game to use buttons for input

--- java.js
+++ java.js
@@ -1,31 +1,65 @@
 let pWins = 0;
 let cWins = 0;
 
-function playRound() {
-  const player = prompt("Enter selection of rock, paper, or scissors: "); 
-  const playerSelection = player.toUpperCase().at(0) + player.toLowerCase().slice(1);
+const rock = document.querySelector('#rock');
+rock.addEventListener('click', () => {
+  console.log(playRound("Rock"));
+});
+
+const paper = document.querySelector('#paper');
+paper.addEventListener('click', () => {
+  console.log(playRound("Paper"));
+});
+
+const scissors = document.querySelector('#scissors');
+scissors.addEventListener('click', () => {
+  console.log(playRound("Scissors"));
+});
+
+const player = document.querySelector('#player');
+const computer = document.querySelector('#computer');
+const result = document.querySelector('#result');
+
+function playRound(selection) {
+  const playerSelection = selection;
   const computerSelection = getComputerChoice ();
 
-  if (playerSelection===computerSelection) {
-    return "It is a tie!";
+  if (pWins < 5 && cWins < 5) {
+    if (playerSelection===computerSelection) {
+      result.textContent = "It is a tie!";
+    }
+    else if ((playerSelection==='Rock' && computerSelection =='Scissors') || 
+    (playerSelection==='Paper' && computerSelection =='Rock') || 
+    (playerSelection==='Scissors' && computerSelection =='Paper')) {
+      ++pWins;
+      player.textContent = "Player Score: " + pWins;
+      result.textContent = "You win! " + playerSelection + " beats " + computerSelection + ".";
+    }
+    else if ((computerSelection==='Rock' && playerSelection =='Scissors') || 
+    (computerSelection==='Paper' && playerSelection =='Rock') || 
+    (computerSelection==='Scissors' && playerSelection =='Paper')) {
+      ++cWins;
+      computer.textContent = "Computer Score: " + cWins;
+      result.textContent = "You lose! " + computerSelection + " beats " + playerSelection + ".";
+    };
   }
-  else if ((playerSelection==='Rock' && computerSelection =='Scissors') || 
-  (playerSelection==='Paper' && computerSelection =='Rock') || 
-  (playerSelection==='Scissors' && computerSelection =='Paper')) {
-    ++pWins;
-    return "You win! " + playerSelection + " beats " + computerSelection;
+  else if (pWins==5) {
+    alert("You win the game! You have won 5 rounds before the computer. Great job!");
+    pWins = 0;
+    cWins = 0;
+    player.textContent = "Player Score: " + pWins;
+    computer.textContent = "Computer Score: " + cWins;
+    result.textContent = ""
   }
-  else if ((computerSelection==='Rock' && playerSelection =='Scissors') || 
-  (computerSelection==='Paper' && playerSelection =='Rock') || 
-  (computerSelection==='Scissors' && playerSelection =='Paper')) {
-    ++cWins;
-    return "You lose! " + computerSelection + " beats " + playerSelection;
+  else if (cWins==5) {
+    alert("You lose the game. The computer has won 5 rounds before you. Please try again.");
+    pWins = 0;
+    cWins = 0;
+    player.textContent = "Player Score: " + pWins;
+    computer.textContent = "Computer Score: " + cWins;
+    result.textContent = ""
   }
-  else {
-    alert(player + " is invalid input. For next game you must enter selection of rock, paper, or scissors.");
-    return "Invalid user input resulted in cancelled round.";
-  }
-}
+};
 
 function getComputerChoice () {
   let n = Math.floor(Math.random()*3);
@@ -38,18 +72,4 @@ function getComputerChoice () {
   else {
     return "Scissors";
   }
-}
-
-function game () {
-  while (pWins<5 && cWins<5) {
-    console.log(playRound());
-  }
-  if (pWins==5) {
-    return console.log("You win the game! You have won 5 rounds before the computer. Great job!");
-  }
-  else {
-    return console.log("You lose the game. The computer has won 5 rounds before you. Please try again.");
-  }
-}
-
-game();
+};
